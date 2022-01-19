deb9a67a17e95f1c393fdc2dac8da1288a6e4d19
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-18 21:05:31 -0500

    Add form to AddContact component and rendered list to ContactList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header'
+import AddContact from './AddContact'
+import ContactList from './ContactList'
 
 function App() {
+
+  const contacts = [
+    {
+      id: "1",
+      name: "John",
+      email: "john@test.com"
+    },
+    {
+      id: "2",
+      name: "Jane",
+      email: "jane@test.com"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
