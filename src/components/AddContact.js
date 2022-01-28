9bb88212809539a2973bcd0d6ccaa3c09870cd95
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-28 16:15:42 -0500

    Add useEffect to retrieve contacts after refresh

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -6,11 +6,24 @@ class AddContact extends Component {
         email: ""
     }
 
+    add = (e) => {
+        e.preventDefault()
+        if(this.state.name === "" || this.state.email === "") {
+            alert("All the fields are mandatory")
+            return
+        }
+        this.props.addContactHandler(this.state)
+        this.setState({
+            name: "",
+            email: ""
+        })
+    }
+
     render() {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
                         <input 
